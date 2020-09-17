c94db2e1cce1653ecf112c30272ec55cd04d39a2
Philippe Batigne <pbatigne@gmail.com>
2020-09-16 20:43:05 -0400

    init work on profile widget

---
+++ src/components/profile/PersonWidget.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+import profile_pic from './profile_pic.png';
+
+const ProfilePicture = styled.div`
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+  }
+`
+
+const ProfileText = styled.div`
+  margin-left: 10px;
+`
+
+const ProfileName = styled.div`
+  display: inline-block;
+  margin-left: 5px;
+  font-weight: bold;
+`
+
+const WidgetWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const PersonWidget = (props) =>(
+  <WidgetWrapper>
+    <ProfilePicture>
+      <img src={profile_pic} alt=""/>
+    </ProfilePicture>
+    <ProfileText>
+      Hello,
+      <ProfileName>Jay</ProfileName>
+    </ProfileText>
+  </WidgetWrapper>
+)
+
+export default PersonWidget;
